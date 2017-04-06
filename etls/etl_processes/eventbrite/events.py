6a0b6c8c39785969b4b6b441edc8f82ab2f8ea64
Francisco Calderon <fcalderonrodriguez@dons.usfca.edu>
2017-04-06 15:31:19 -0700

    update structure

--- etls/etl_processes/eventbrite/events.py
+++ etls/etl_processes/eventbrite/events.py
@@ -1,17 +1,31 @@
 
 import requests
+import datetime
 
-city = "New York City"
-city = city.replace(" ", "+")
-url = "https://www.eventbriteapi.com/v3/events/search/?q="+city
-response = requests.get(
-    url,
-    headers = {
-        "Authorization": "Bearer FOFJGDOSGZSEJTYSHPAX",
-    },
-    verify = True,  # Verify SSL certificate
-)
 
-for index in range(10):
-    print response.json()['events'][index]['name']['text']
-    print response.json()['events'][0]['start']['utc']
+def get_data():
+    cities = ['New York, NY','Austin, TX','San Francisco, CA','Boston, MA','Chicago, IL','Miami, FL']
+
+    res_list = ['events']
+    for city in cities:
+        city = city.replace(" ", "+")
+        print city
+        url = "https://www.eventbriteapi.com/v3/events/search/?q="+city
+        response = requests.get(
+            url,
+            headers = {
+                "Authorization": "Bearer FOFJGDOSGZSEJTYSHPAX",
+            },
+            verify = True,  # Verify SSL certificate
+        )
+        for index in response.json()['events']:
+            event_dict = {}
+            event_dict['city'] = city
+            event_dict['event_name'] = index['name']['text']
+            event_dict['event_description'] = index['description']['text']
+            event_dict['event_date'] = str(datetime.datetime.strptime(index['start']['utc'], "%Y-%m-%dT%H:%M:%SZ").date())
+            res_list.append(event_dict)
+
+
+if __name__ == "__main__":
+    get_data()
