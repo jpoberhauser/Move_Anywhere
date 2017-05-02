d4a1090d2599c1ab3f803a9d2f95028fee33ae6e
jpoberhauser <jpoberhauser@gmail.com>
2017-05-01 20:30:26 -0700

    latest app_boot

--- flask_app_bootstrap/app_boot.py
+++ flask_app_bootstrap/app_boot.py
@@ -34,6 +34,16 @@ def input_form():
     """
     return flask.render_template("input_form.html")
 
+@app.route("/NYC")
+def show_j_nyc():
+    cur.execute("""SELECT city,title,company, snippet from jobs where city='New York' limit 10""")
+    jobs = pd.DataFrame(cur.fetchall(), columns=[ 'city',
+    'title','company','snippet'])
+
+    return flask.render_template('view.html',tables=[jobs.to_html(classes='jobs'),events.to_html(classes='events'),
+    schools.to_html(classes='schools')],
+    titles = ['na', 'Jobs', 'Events','Schools'])
+
 @app.route("/tables")
 def show_tables():
     return flask.render_template('view.html',tables=[jobs.to_html(classes='jobs'),events.to_html(classes='events'),
@@ -46,27 +56,23 @@ if __name__ == "__main__":
     conn = psycopg2.connect("port=5432 dbname='numinous' user='stanley' host='appdev.cuq8d0oymhgs.us-west-2.rds.amazonaws.com' password='Msan4ever!'")
     cur = conn.cursor()
     #cur.execute("""SELECT datname from pg_database""")
-    cur.execute("""SELECT * from jobs""")
-    jobs = pd.DataFrame(cur.fetchall(), columns=['id', 'city', 'job_id',
-    'title','url','company','snippet','state','date_posted'])
-
 
     cur = conn.cursor()
     #cur.execute("""SELECT datname from pg_database""")
-    cur.execute("""SELECT * from events""")
+    cur.execute("""SELECT * from events limit 100""")
     events = pd.DataFrame(cur.fetchall(), columns=['event_name', 'city', 'event_description',
     'event_date','date_added'])
 
 
     cur = conn.cursor()
     #cur.execute("""SELECT datname from pg_database""")
-    cur.execute("""SELECT * from schools""")
+    cur.execute("""SELECT * from schools limit 100""")
     schools = pd.DataFrame(cur.fetchall(), columns=['id', 'city','name', 'phone',
     'reviewslink','enrollment','parentrating','lon','overviewlink','gsid','state','address',
     'lat','type','graderange','ratingslink'])
 
 
-    conn.close()
+
     # Open a web browser pointing at the app.
     os.system("open http://localhost:{0}".format(port))
     # Set up the development server on port 8000.
