11f74ec32c68de321a1ff0a1889da9ffca049a93
jpoberhauser <jpoberhauser@gmail.com>
2017-06-18 11:50:00 -0700

    adds python code and different versions of text

--- flask_app_bootstrap/app_boot.py
+++ flask_app_bootstrap/app_boot.py
@@ -18,8 +18,29 @@ def home_page():
     """
     When you request the root path, flask will return the landing page  html.
     """
-    return flask.render_template("index.html")
+    return flask.render_template("index2.html")
 
+@app.route("/v2")
+def home_page2():
+    """
+    When you request the root path, flask will return the landing page  html.
+    """
+    return flask.render_template("index2.html")
+
+@app.route("/v3")
+def home_page3():
+    """
+    When you request the root path, flask will return the landing page  html.
+    """
+    return flask.render_template("index3.html")
+
+
+@app.route("/v4")
+def home_page4():
+    """
+    When you request the root path, flask will return the landing page  html.
+    """
+    return flask.render_template("index4.html")
 
 @app.route("/input_info")
 def input_form():
